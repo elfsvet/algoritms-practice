bc17f688a21bb76ebf17a739a5c8d570baf9be35
elfsvet <stepanmatysik@gmail.com>
2022-05-08 22:44:41 -0400

    added camel-case starting code

--- Unsolved/camel-case.js
+++ Unsolved/camel-case.js
@@ -1,5 +1,7 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {
-    
+var camelCase = function (str) {
+    //return a camel case version of the given string
+  
 };
+camelCase("     LET THE FORCE BE WITH YOU     ");
